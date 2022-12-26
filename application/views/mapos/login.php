b6dacb8a42178d93d306719625266537547a9851
Wal sabioni <113319667+walsabioni@users.noreply.github.com>
2022-12-26 05:34:18 -0300

    Update login.php

--- application/views/mapos/login.php
+++ application/views/mapos/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<title><?= $this->config->item('app_name') ?> </title>
+<title>WSYSTEM</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.min.css" />
